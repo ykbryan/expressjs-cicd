5e952b741df2702c4cf281ab56c0c76328efaf46
Bryan Chua <me@bryanchua.com>
2019-05-11 01:57:43 +0800

    Add e2d

---
+++ e2e.js
@@ -0,0 +1,22 @@
+// Import requirement packages
+require('chromedriver');
+const assert = require('assert');
+const { Builder, Key, By, until } = require('selenium-webdriver');
+describe('Checkout QA site', function () {
+  let driver;
+  before(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+  // Next, we will write steps for our test. 
+  // For the element ID, you can find it by open the browser inspect feature.
+  it('Hello World', async function () {
+    await driver.get('http://expressjs-qa.ap-southeast-1.elasticbeanstalk.com/');
+    // check content
+    await driver.findElement(By.css('body')).getText().then(function (text) {
+      assert.equal(text, "Hello World!");
+    });
+
+  });
+  // close the browser after running tests
+  after(() => driver && driver.quit());
+})
