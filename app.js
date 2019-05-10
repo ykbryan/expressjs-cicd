495bce4e3d35ab0361fd7b47d6976fad7f0a9508
Bryan Chua <me@bryanchua.com>
2019-05-10 17:57:22 +0800

    Code check

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
